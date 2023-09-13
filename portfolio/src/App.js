890fc87d2e29e9bb97369cc224cc058babe73d22
Osc-53219 <102975104+Osc-53219@users.noreply.github.com>
2023-09-13 17:06:07 -0500

    Added NavBar titles and styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="text-4xl font-bold font-signature">Hello</h1>
+        <NavBar />
       </div>
     );
   }
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { FaBars, FaTimes } from "react-icons/fa"
 
 const NavBar = () => {
+
+// Adding an Array of links instead of making a long list for the NavBar 
+
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            link: 'experience'
+        },
+        {
+            id: 1,
+            link: 'contact'
+        },
+    ]
+
   return (
-    <div>NavBar</div>
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+        <div>
+            <h1 className='text-5xl font-signature ml-2'>Oscar</h1>
+        </div>
+
+        <ul className='flex'>
+            
+        {/* // Whenever your looping an array the elements update based on the key in which we are adding below */}
+
+        {links.map(({ id, link }) => (
+            <li 
+                key={id} 
+                className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
+
+                {link}
+            </li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
