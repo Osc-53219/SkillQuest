b23bf0c6ac739c53d8b6166ac90af5a05d5d1fda
Osc-53219 <102975104+Osc-53219@users.noreply.github.com>
2023-09-13 16:13:32 -0500

    Added react-icons and custom fonts

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h1 className="text-4xl font-bold font-signature">Hello</h1>
       </div>
     );
   }
