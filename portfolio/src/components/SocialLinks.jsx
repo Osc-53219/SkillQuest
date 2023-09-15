b2041f68529876d7850f3df5e2a18e88b820f5d6
Osc-53219 <102975104+Osc-53219@users.noreply.github.com>
2023-09-15 00:09:14 -0500

    Finished array that loops through different links

--- portfolio/src/components/SocialLinks.jsx
+++ portfolio/src/components/SocialLinks.jsx
@@ -20,7 +20,7 @@ const SocialLinks = () => {
             id:2,
             child: (
                 <>
-                LinkedIn <FaGithub size={30}/>
+                GitHub <FaGithub size={30}/>
             </>
             ),
             href: 'https://github.com/Osc-53219',
@@ -29,7 +29,7 @@ const SocialLinks = () => {
             id:3,
             child: (
                 <>
-                LinkedIn <HiOutlineMail size={30}/>
+                Mail <HiOutlineMail size={30}/>
             </>
             ),
             href: 'mailto:foo@gmail.com',
@@ -38,27 +38,35 @@ const SocialLinks = () => {
             id:4,
             child: (
                 <>
-                LinkedIn <BsFillPersonLinesFill size={30}/>
+                Resume <BsFillPersonLinesFill size={30}/>
             </>
             ),
             href: '/resume.pdf',
             style: 'rounded-br-md',
             download: true
         },
-    ]
+    ];
 
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
         <ul>
-            <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'>
+            {links.map(({id, child, href, style, download}) => (
+                <li 
+                key={id} 
+                className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + style}
+                >
                 <a 
-                href=''
-                className='flex justify-between items-center w-full text-white'>
-                    <>
-                    LinkedIn <FaLinkedin size={30}/>
-                    </>
+                href={href}
+                className='flex justify-between items-center w-full text-white'
+                download={download}
+                target='_blank'
+                >
+                    {child}
                 </a>
             </li>
+            ))}
+
+            
         </ul>
     </div>
   );
